2c22b9de10cf228b49f229b7651be6adac487951
Axel Meyrek Mac <chavezsud1406@gmail.com>
2025-02-19 23:08:48 -0600

    Ultimate Point

--- Empleado.java
+++ Empleado.java
@@ -1,10 +1,13 @@
 
 public class Empleado extends Persona {
-    private String numeroEmpleado;
     private String puesto;
     private double salario;
     private int turno;
     private Prestamo prestamoGestionado;
+    private String especialidad;
+    private String horario;
+
+
     private static int contadorId = 0;
     
     public static final int MATUTINO = 0;
@@ -41,8 +44,23 @@ public class Empleado extends Persona {
     }
 
     public void setTurno(int turno) {
+        if (turno < MATUTINO || turno > MIXTO) return;
+        
         this.turno = turno;
+    
+        switch (turno) {
+            case MATUTINO:
+                this.horario = "08:00 - 14:00";
+                break;
+            case VESPERTINO:
+                this.horario = "14:00 - 20:00";
+                break;
+            case MIXTO:
+                this.horario = "08:00 - 12:00 y 16:00 - 20:00";
+                break;
+        }
     }
+    
 
     public Prestamo getPrestamoGestionado() {
         return prestamoGestionado;
@@ -83,4 +101,18 @@ public class Empleado extends Persona {
                ", nombre=" + getNombre() + 
                ", id=" + getId() + "]";
     }
+
+    /* MY CHANGES */
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
 }
